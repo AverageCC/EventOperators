34de8e0c2e5c62e2cfa4b260ad78941a283be1b6
AverageCC <reddutv@gmail.com>
2024-04-16 12:46:37 -0700

    Note update

--- events.js
+++ events.js
@@ -1,33 +1,30 @@
-let dx = 2; // Change in x direction
-let dy = 2; // Change in y direction
-let intervalId; // ID of the interval
+let dx = 2; // Defines the change in the x direction
+let dy = 2; // Defines the change in the y direction
+let intervalId; // This sets the interval for the movement as a global variable
 
-const img = document.getElementById('myImage');
+const img = document.getElementById('dvdimage'); // Get the image element, which has the id 'dvdimage' show on line defined on line 6 of strings.html
 
-function startMovement() {
-    // Only start a new interval if one is not already running
-    if (!intervalId) {
-        intervalId = setInterval(() => {
-            let x = img.offsetLeft;
-            let y = img.offsetTop;
+function startMovement() { // startMovement function is executed when the start button is clicked
+    if (!intervalId) { // If the intervalId is not set show using the ! before the intervalId, then it sets the intervalId
+        intervalId = setInterval(() => { // Set the interval
+            let x = img.offsetLeft; // Gets the x position of the image
+            let y = img.offsetTop; // Gets the y position of the image
 
-            // Check if the image hit the edge of the screen
-            if (x + img.offsetWidth > window.innerWidth || x < 0) dx = -dx;
-            if (y + img.offsetHeight > window.innerHeight || y < 0) dy = -dy;
+            if (x + img.offsetWidth > window.innerWidth || x < 0) dx = -dx; // If the image hits the right or left side of the screen, then change the direction of the image
+            if (y + img.offsetHeight > window.innerHeight || y < 0) dy = -dy; // If the image hits the top or bottom of the screen, then change the direction of the image
 
-            // Move the image
-            img.style.left = x + dx + 'px';
-            img.style.top = y + dy + 'px';
-        }, 20); // 20 milliseconds = 50 frames per second
+            img.style.left = x + dx + 'px';  // Moves the image in the x direction. The x + dx is the new position of the image while the 'px' is the unit of measurement
+            img.style.top = y + dy + 'px'; // Moves the image in the y direction. The y + dy is the new position of the image while the 'px' is the unit of measurement
+        }, 20); // 20 milliseconds = 50 frames per second (fps). The 20 is inside the setInterval function on line 9 - setInterval(() => { ... }, 20);
     }
 }
 
-function stopMovement() {
-    if (intervalId) {
+function stopMovement() { // stopMovement function is executed when the stop button is clicked
+    if (intervalId) { // Checks to see if the intervalId is set
         clearInterval(intervalId); // Clear the interval
-        intervalId = null;
+        intervalId = null; // Reset the intervalId
     }
     
-    img.style.left = '0px'; // Reset image position
-    img.style.top = '0px';
+    img.style.left = '0px'; // Resets the image position to the top left corner
+    img.style.top = '0px'; // Resets the image position to the top left corner
 }
